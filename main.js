148725031efe0cb805068826de0435b1bcc43130
Wenjun Che <wenjun@openfin.co>
2018-05-22 19:01:51 -0400

    Repo for https://github.com/electron/electron/issues/13043

--- main.js
+++ main.js
@@ -13,7 +13,9 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+//  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1024, height: 880, x: 30, y: 30,
+    resizable: false })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
